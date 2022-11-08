95bfde003a8ccd118adfeb09931cf28b94e019e9
Kevwe Gloria Okpoh <Kevweglo@gmail.com>
2022-11-08 15:48:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+$(".dropbtn").click(function() {
+    $("#myDropdown").togleClass("show");
+})
+
+function myFunct() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+  }
+  
+  function filterFunction1() {
+    var input, filter, i;
+    input = document.getElementById("myInput1");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown1");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
